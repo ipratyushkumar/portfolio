150771605cf840f8c28bce40993cf75741f5e4b2
Pratyush Kumar <thepratyushsingh01@gmail.com>
2026-08-30 23:12:41 +0530

    Fix certificate URLs

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -4,7 +4,7 @@ const certifications = [
     name: "Introduction to Data Science",
     issuer: "Cisco Networking Academy",
     year: "Completed",
-    certificateUrl: "public/certificates/introductio-data-science.pdf",
+    certificateUrl: "/certificates/introductio-data-science.pdf",
   },
 
   {
@@ -12,7 +12,7 @@ const certifications = [
     name: "Introduction to Artificial Intelligence and Machine Learning",
     issuer: "Board Infinity",
     year: "Completed",
-    certificateUrl: "public/certificates/introduction-ai-ml.pdf",
+    certificateUrl: "/certificates/introduction-ai-ml.pdf",
   },
 
   {
@@ -20,7 +20,7 @@ const certifications = [
     name: "Data Science for Beginners",
     issuer: "Board Infinity",
     year: "Completed",
-    certificateUrl: "public/certificates/data-science-for-beginners.pdf",
+    certificateUrl: "/certificates/data-science-for-beginners.pdf",
   },
 
   {
@@ -28,7 +28,7 @@ const certifications = [
     name: "Effective Time Management",
     issuer: "Techveda",
     year: "2025",
-    certificateUrl: "public/certificates/effective-time-management.png",
+    certificateUrl: "/certificates/effective-time-management.png",
   },
 ];
 
